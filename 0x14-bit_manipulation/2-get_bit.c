8ffb3d17d50200538ed82d32a6bdc852ac2dbc3a
maruci47 <maria.wk447@gmail.com>
2023-04-06 12:32:51 +0000

    bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,18 +10,11 @@
  *        */
 int get_bit(unsigned long int n, unsigned int index)
 {
-        unsigned int i;
+        if (index >= (sizeof(unsigned long int) * 8))
+                    return (-1);
 
-            if (n == 0 && index < 64)
+            if ((n & (1 << index)) == 0)
                         return (0);
 
-                for (i = 0; i <= 63; n >>= 1, i++)
-                        {
-                                    if (index == i)
-                                                {
-                                                                return (n & 1);
-                                                                        }
-                                        }
-
-                    return (-1);
+                return (1);
 }
