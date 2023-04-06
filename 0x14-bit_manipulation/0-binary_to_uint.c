818c7fc6afc9b4fdd31162755c7308d8d894ab98
maruci47 <maria.wk447@gmail.com>
2023-04-06 11:51:47 +0000

    skate

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,62 +1,37 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- *  * binary_to_uint - function that converts
- *   * @b: is pointing to a string of 0 and 1 chars
- *    * Return: unsigned integer, converted number, or 0
- *     */
+ *  * binary_to_uint - converts a binary number to an
+ *   * unsigned int.
+ *    * @b: binary.
+ *     *
+ *      * Return: unsigned int.
+ *       */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int change = 0, total = 0;
-            int i, len = 0;
+        unsigned int ui;
+            int len, base_two;
 
-                if (b == NULL)
+                if (!b)
                             return (0);
 
-                    len = _strlen(b);
-                        i = 0;
+                    ui = 0;
 
-                            while (b[i] != '\0')
+                        for (len = 0; b[len] != '\0'; len++)
+                                    ;
+
+                            for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
                                     {
-                                                if ((b[i] != '0' && b[i] != '1'))
-                                                                return (0);
-                                                        change = (b[i] - 48) * _pow_recursion(2, (len - 1) - i);
-                                                                total += change;
-                                                                        i++;
-                                                                            }
-                                return (total);
-}
+                                                if (b[len] != '0' && b[len] != '1')
+                                                            {
+                                                                            return (0);
+                                                                                    }
 
-/**
- *  * _strlen - function that returns the length of a string
- *   * @s: parameter defined in main
- *    * Return: length of string
- *     */
-int _strlen(const char *s)
-{
-        int x = 0;
+                                                        if (b[len] & 1)
+                                                                    {
+                                                                                    ui += base_two;
+                                                                                            }
+                                                            }
 
-            while (*s != '\0')
-                    {
-                                x++;
-                                        s++;
-                                            }
-                return (x);
-}
-
-/**
- *  * _pow_recursion - function that returns x raised to the power of y
- *   * @x: parameter defined in main, integer
- *    * @y: parameter defined in main, exponent
- *     *
- *      * Return: integer
- *       */
-int _pow_recursion(int x, int y)
-{
-        if (y < 0)
-                    return (-1);
-            if (y == 0)
-                        return (1);
-                return (x * _pow_recursion(x, y - 1));
+                                return (ui);
 }
